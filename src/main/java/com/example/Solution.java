a22beafb813ff885d4f8ad13016ca37af2943ff9
Bayakan <spring.boot@hotmail.com>
2024-05-26 13:16:27 +0300

    Java Stdin and Stdout I

---
+++ src/main/java/com/example/Solution.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String b = scan.nextLine();
+        int a = scan.nextInt();
+        // Complete this line
+        // Complete this line
+
+        System.out.println("myString is: " + b);
+        System.out.println("myInt is: " + a);
+        // Complete this line
+        // Complete this line
+    }
+}
