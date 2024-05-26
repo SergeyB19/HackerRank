1fa0d2f5ce489cefc7e27878e84fdff9bcab7f0a
Bayakan <spring.boot@hotmail.com>
2024-05-26 13:23:37 +0300

    Welcome to Java!

---
+++ src/main/java/com/example/Welcome_to_Java/Solution.java
@@ -0,0 +1,16 @@
+package com.example.Welcome_to_Java;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+        String b = scanner.nextLine();
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
