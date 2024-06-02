25da13ef9fbe04c985f5006426832d884805212e
Bayakan <spring.boot@hotmail.com>
2024-06-02 21:30:41 +0300

    Практика массивов

---
+++ src/main/java/com/example/dmdev/task/Task2.java
@@ -0,0 +1,20 @@
+package com.example.dmdev.task;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int[] values = new int[100];
+        fillArray(values);
+        Task1.printArray(values);
+
+    }
+
+    public static void fillArray(int[] values) {
+//        int currentIndex = 0;
+        for (int currentValue = 1, currentIndex = 0; currentIndex < values.length; currentValue++) {
+            if (currentValue % 13 == 0 || currentValue % 17 == 0) {
+                values[currentIndex++] = currentValue;
+//                currentIndex++;
+            }
+        }
+    }
+}
